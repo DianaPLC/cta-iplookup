a1c74510ec492bb42da3c8bea52ecbab620cee7f
Diana PLC <dianapatton1@gmail.com>
2025-03-07 15:43:24 -0500

    initial basic prototype

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,48 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [ip, setIp] = useState('');
+  // Preload standard ones?
+  const [fieldList, setFieldList] = useState([]);
+  // Is there a good default here? Or require selection before any data is displayed?
+  // Maybe disable this on error
+  const [field, setField] = useState('region');
+  const [data, setData] = useState({});
+  const [status, setStatus] = useState(null);
+  const [respError, setRespError] = useState('');
+  // add field errors, text, labels, etc.
+
+  const BASE_URL = `http://ip-api.com/json/`;
+
+  function retrieveData() {
+    fetch(BASE_URL + ip)
+      .then(response => {
+        if (!response.ok) throw new Error(response);
+        return response.json()
+      })
+      .then(parsed => {
+          if (parsed.status != 'success') throw new Error(parsed.message);
+          setData(parsed);
+          setStatus(parsed.status)
+          // Filter out the redundant/unhelpful ones?
+          setFieldList(Object.keys(parsed));
+      })
+      .catch(error => {
+        // Should get more fine-grained here
+        setStatus(error.message);
+        setRespError(`Unable to look up data for ${ip}; please double-check the address.`)
+      });
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input type='text' onChange={(e) => {setIp(e.target.value)}} onBlur={retrieveData}/>
+      <select onChange={(e) => setField(e.target.value)}>
+        {fieldList.map((f,i) => <option key={i} value={f}>{f}</option>)}
+      </select>
+      {status && <div>{status}</div>}
+      <div>{status == 'success' ? data[field] : respError}</div>
     </>
   )
 }
