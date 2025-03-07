cee84eee6d0047eed7ab08462a279bcfaffd4d61
Diana PLC <dianapatton1@gmail.com>
2025-03-07 17:49:15 -0500

    tighten up behavior and display

--- src/App.jsx
+++ src/App.jsx
@@ -3,43 +3,63 @@ import './App.scss'
 
 function App() {
   const [ip, setIp] = useState('');
-  // Preload standard ones?
-  const [fieldList, setFieldList] = useState(['country','countryCode','region','regionName','city','zip','lat','lon','timezone','isp','org','as']);
-  // Is there a good default here? Or require selection before any data is displayed?
-  // Maybe disable this on error
   const [field, setField] = useState('');
   const [data, setData] = useState({});
   const [status, setStatus] = useState(null);
   const [respError, setRespError] = useState('');
-  // add field errors, text, labels, etc.
   const [ipError, setIpError] = useState(null);
   const [fieldError, setFieldError] = useState(null);
 
   const BASE_URL = `http://ip-api.com/json/`;
   const IPV4 = /^([0-9]{1,3}\.){3}[0-9]{1,3}$/
-  // modify the following to ensure it can't end in a singular colon
-  // also require the right number of possible segments? ie total can't exceed 8
-  const IPV6 = /^(([0-9a-f]\:){7}[0-9a-f])|([0-9a-f]\:)*(\:\:)([0-9a-f]\:)*([0-9a-f]){0,1}$/i
-  const IPV6_DUAL = /^(([0-9a-f]\:){7}[0-9a-f])|([0-9a-f]\:)*(\:\:)([0-9a-f]\:)*([0-9]{1,3}\.){3}([0-9]){1,3}$/
+  // not a perfect ipv6 check, but errors will be reported back by the API
+  const IPV6 = /^(([0-9a-f]{1,4}\:){7}[0-9a-f]{1,4})|([0-9a-f]{1,4}\:)*(\:\:)([0-9a-f]{1,4}\:)*([0-9a-f]{1,4}){0,1}$/i
+  const IPV6_DUAL = /^(([0-9a-f]{1,4}\:){7}[0-9a-f]{1,4})|([0-9a-f]{1,4}\:)*(\:\:)([0-9a-f]{1,4}\:)*([0-9]{1,3}\.){3}([0-9]){1,3}$/i
+
+  const FIELDS = new Map([
+    ['country', 'Country'],
+    ['countryCode', 'Country Code'],
+    ['region', 'Region Code'],
+    ['regionName', 'Region Name'],
+    ['city', 'City'],
+    ['zip', 'Zip Code'],
+    ['lat', 'Latitude'],
+    ['lon', 'Longitude'],
+    ['timezone', 'Time Zone'],
+    ['isp', 'ISP'],
+    ['org', 'Organization Name'],
+    ['as', 'AS Number and Organization']
+  ]);
 
   const updateIp = (val) => {
     setIp(val);
     setIpError(null);
+    setRespError(null);
   }
 
   const updateField = (val) => {
-    if (fieldList.indexOf(val) == -1) {
-      setFieldError('Please select info from the list provided')
-      setField('')
-    } else {
+    if (FIELDS.has(val)) {
       setFieldError(null);
       setField(val);
+    } else {
+      setFieldError('Please select info from the list provided');
+      setField('');
     }
   }
 
   const retrieveData = () => {
+    if (!ip.length) {
+      setIpError('Please enter a value for the IP address');
+      return;
+    }
     if (!IPV4.test(ip) && !IPV6.test(ip) && !IPV6_DUAL.test(ip)) {
-      setIpError(`${ip} is not in a valid format. Acceptable formats include IPv4 (eg. 1.234.5.67), IPv6 (eg. ffff::01234), and IPv6 dual (eg. ffff::01234:1.234.5.67)`)
+      setIpError(`
+        ${ip} is not in a valid format.
+        Acceptable formats include:
+        IPv4 (eg. 1.234.5.67)
+        IPv6 (eg. ffff::01234)
+        IPv6 dual (eg. ffff::01234:1.234.5.67)
+      `)
       return;
     }
     fetch(BASE_URL + ip)
@@ -51,13 +71,15 @@ function App() {
           if (parsed.status != 'success') throw new Error(parsed.message);
           setData(parsed);
           setStatus(parsed.status)
-          // Filter out the redundant/unhelpful ones?
-          setFieldList(Object.keys(parsed));
+          console.log(parsed)
       })
       .catch(error => {
         // Should get more fine-grained here
         setStatus(error.message);
-        setRespError(`Unable to look up data for ${ip}; please double-check the address.`)
+        setRespError(`
+          Unable to look up data for ${ip};
+          please double-check the address.
+        `)
       });
   }
 
@@ -65,7 +87,9 @@ function App() {
     <>
       <h1>IP Address Data Lookup</h1>
       <div className='input-container'>
-        <label htmlFor='ip' className={ipError ? 'error' : ''}>Enter an IP address:</label>
+        <label htmlFor='ip' className={ipError ? 'error' : ''}>
+          Enter an IP address:
+        </label>
         <input
           id='ip'
           name='ip'
@@ -77,7 +101,9 @@ function App() {
         />
         {ipError && <div className='error'>{ipError}</div>}
 
-        <label htmlFor='field' className={fieldError ? 'error' : ''}>Select the address info to view:</label>
+        <label htmlFor='field' className={fieldError ? 'error' : ''}>
+          Select the address info to view:
+        </label>
         <select
           id='field'
           name='field'
@@ -86,18 +112,22 @@ function App() {
           defaultValue={field}
         >
           <option value='' disabled></option>
-          {fieldList.map((f,i) => 
-            <option key={i} value={f}>{f}</option>
+          {Array.of(...FIELDS).map((entry, index) => 
+            <option key={index} value={entry[0]}>{entry[1]}</option>
           )}
         </select>
         {fieldError && <div className='error'>{fieldError}</div>}
       </div>
 
-      {/* {status && <div className='status'>{status}</div>} */}
-      <div className='data'>
-        <h2>{respError ? status : field}</h2>
-        {status == 'success' ? data[field] : respError}
-      </div>
+      { (status && field) && 
+        <div className='data'>
+          <h2>{respError ? status : FIELDS.get(field)}</h2>
+          {status == 'success' ?
+            (data[field].length > 0 ? data[field] : 'unknown')
+            : respError
+          }
+        </div>
+      }
     </>
   )
 }
